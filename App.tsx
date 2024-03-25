ff3ae0000b50254ae32090247f7ffc1f3b389022
Enzo-1022 <enzofernandes560@gmail.com>
2024-03-25 20:25:49 -0300

    criei o app

--- App.tsx
+++ App.tsx
@@ -1,10 +1,27 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from "react";
+import { StyleSheet, Text, TextInput, View } from 'react-native';
 
 export default function App() {
+
+  const [ baseTriangulo, setBase] = useState('');
+
+  const [ alturaTriangulo, setAltura ] = useState('');
+
+  const calculoAreaTriangulo = (parseFloat(baseTriangulo) * parseFloat(alturaTriangulo)) / 2;
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text >Calcular Area do triangulo </Text>
+
+      <Text>Informe a area do triangulo</Text>
+      <TextInput onChangeText={setBase} keyboardType="numeric" style={styles.Input}/>
+
+      <Text>Informe a Altura do triangulo</Text>
+      <TextInput onChangeText={setAltura} keyboardType="numeric" style={styles.Input}/>
+
+
+      <Text>{calculoAreaTriangulo}</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -17,4 +34,12 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  Label: {
+    
+  },
+  Input: {
+    width: '90%',
+    borderWidth: 2,
+    padding: 5
+  }
 });
